a1ccd0118c7903fac297abf314c3cd27beba9fce
puspalata123 <puspalatarout4@gmail.com>
2022-02-17 20:09:37 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p>I am learning React. My life is getting better.</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
